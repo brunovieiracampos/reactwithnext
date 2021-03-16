d3967ab3ffc5f8c6f34c85f1de2e38bab5e5557f
Bruno Campos <tp.bruno.campos@totvs.com>
2021-03-16 15:02:05 -0300

    array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
 
     state = {
-      name: 'Bruno Vieira Campos',
-      counter: 0
+      posts : [
+        {
+          id: 1,
+          title: 'Título 1',
+          body: 'corpo do título 1'
+        },
+        {
+          id: 2,
+          title: 'Título 2',
+          body: 'corpo do título 2'
+        },
+        {
+          id: 3,
+          title: 'Título 3',
+          body: 'corpo do título 3'
+        }
+      ]
     };
 
   handlePClick = () => {
@@ -21,25 +35,16 @@ class App extends Component {
   }
 
   render(){
-    const { name, counter } = this.state; 
+    const { posts } = this.state; 
 
       return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p onClick={this.handlePClick}>
-              { name }
-            </p>
-            <a
-              onClick={this.handleAClick}
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Este é o contador {counter}
-            </a>
-          </header>
+          {posts.map(post => (
+            <div key={post.id}>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          ))}
         </div>
     );
   }
