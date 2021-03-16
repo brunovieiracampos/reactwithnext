f7b13d57c35bfcd8efdfed2d997c470a8ebe37b8
Bruno Campos <tp.bruno.campos@totvs.com>
2021-03-15 22:44:55 -0300

    remove constructor and super, add class fields

--- src/App.js
+++ src/App.js
@@ -4,32 +4,40 @@ import { Component } from 'react';
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
-
-    this.state = {
-      name: 'Bruno Vieira Campos'
+    state = {
+      name: 'Bruno Vieira Campos',
+      counter: 0
     };
 
+  handlePClick = () => {
+    this.setState({ name: 'Vieira Bruno' });
+  }
+
+  handleAClick = (event) =>{
+    event.preventDefault();
+    const { counter } = this.state;
+    
+    this.setState( {counter: counter + 1} );
   }
 
   render(){
-    const { name } = this.state; 
+    const { name, counter } = this.state; 
 
       return (
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p onClick={() => console.log('<p> clicado')}>
+            <p onClick={this.handlePClick}>
               { name }
             </p>
             <a
+              onClick={this.handleAClick}
               className="App-link"
               href="https://reactjs.org"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Learn React
+              Este é o contador {counter}
             </a>
           </header>
         </div>
