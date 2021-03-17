42e41712714700176611df121d6beb7a9869466d
Bruno Campos <tp.bruno.campos@totvs.com>
2021-03-17 15:09:16 -0300

    data fetch

--- src/App.js
+++ src/App.js
@@ -4,22 +4,7 @@ import { Component } from 'react';
 class App extends Component {
 
     state = {
-      counter: 0,
-      posts : [{
-        id: 1,
-        title: 'Título 1',
-        body: 'corpo do título 1'
-      },
-      {
-        id: 2,
-        title: 'Título 2',
-        body: 'corpo do título 2'
-      },
-      {
-        id: 3,
-        title: 'Título 3',
-        body: 'corpo do título 3'
-      }]
+      posts : []
     };
 
   handlePClick = () => {
@@ -34,47 +19,46 @@ class App extends Component {
   };
 
   componentDidMount(){
-    this.handleTimeout();
+    this.loadPosts();
+   
   };
 
-  timeoutUpdate = null;
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+    
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
 
-  componentDidUpdate(){
-    this.handleTimeout();
-  }
+    const postsJson =  await posts.json();
+    const photosJson =  await photos.json();
 
-  componentWillUnmount(){
-    clearTimeout(this.timeoutUpdate);
-  }
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
 
-  handleTimeout = () => {
-    const {posts, counter } = this.state;
-    posts[0].title = 'O título mudou';
-
-    this.timeoutUpdate = setTimeout(() => {
-      this.setState({
-        posts,
-        counter : counter + 1
-      })
-    }, 2000);
+    this.setState({posts : postsAndPhotos});
   }
 
   render(){
-    const { posts, counter } = this.state; 
+    const { posts } = this.state; 
 
       return (
-        <div className="App">
-          <p>{counter}</p>
-          {posts.map(post => (
-            <div key={post.id}>
-              <h1>{post.title}</h1>
-              <p>{post.body}</p>
-            </div>
-          ))}
-        </div>
+        <section className="container">
+          <div className="App" className="posts">
+            {posts.map(post => (
+              <div className="post">
+                <img src={post.cover} alt={post.title} />
+                <div key={post.id} className="post-content">
+                  <h1>{post.title}</h1>
+                  <p>{post.body}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+        
     );
   }
-
 }
 
 export default App;
