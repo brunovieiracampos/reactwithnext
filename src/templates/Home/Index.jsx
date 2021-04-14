0751dd422fd044df78d78278d9f4b02174ca5d97
Bruno Campos <tp.bruno.campos@totvs.com>
2021-04-14 20:40:18 -0300

    create nem component

--- src/templates/Home/Index.jsx
+++ src/templates/Home/Index.jsx
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import { loadPosts } from '../../utils/load-posts';
 import { Posts } from '../../components/Posts';
 import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
 
 export class Home extends Component {
 
@@ -70,27 +71,38 @@ export class Home extends Component {
     } = this.state; 
     const noMorePosts = page + postsPerPage >= allPosts.length;
 
+    const filteredPosts = !!searchValue ? 
+    allPosts.filter(
+      post => {
+        return post.title
+        .toLowerCase()
+        .includes(searchValue.toLowerCase());
+      }) : posts;
+
       return (
 
         <section className="container">
-          {
-            //verdadeiro
-            !!searchValue && 
+          
+          <div className="search-container">
+            
+            {//verdadeiro
+              !!searchValue && 
               (
-                <>
                   <h1>Search Value: {searchValue}</h1>
-                  <br />
-                </>
-              )
-          }
-          <input 
-            type="search" 
-            onChange={this.handleChange}
-            value={searchValue}/>
-          <br />
-          <br />
-          <br />
-          <Posts posts = {posts} />
+            )}
+        
+            <TextInput searchValue={searchValue} handleChange={this.handleChange} />
+          
+          </div>
+
+          {filteredPosts.length > 0 && (
+            <Posts posts = {filteredPosts} />
+          )}
+
+          {filteredPosts.length === 0 && (
+            <p>Não existem posts =( </p>
+          )}
+
           <div className="button-container">
             {
               //falso
